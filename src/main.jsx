7974455e6032ee7c107febf69e45d79e12e3ed6f
krati2248 <184264500+krati2248@users.noreply.github.com>
2025-11-22 19:12:42 +0530

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './App/store.js'
+import { store } from './app/store.js'
 import 'react-toastify/dist/ReactToastify.css';
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
